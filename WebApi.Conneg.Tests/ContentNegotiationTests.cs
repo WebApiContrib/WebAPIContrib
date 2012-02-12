aa8c01e7cee8993871b8cf823fdcc5c78a4b0d84
panesofglass <ryan.riley@panesofglass.com>
2012-02-12 00:15:44 -0800

    Initial implementation and cleanup following pairing session with Glenn.

---
+++ WebApi.Conneg.Tests/ContentNegotiationTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using NUnit.Framework;
+
+namespace WebApi.Conneg.Tests {
+	public static class UnitTest {
+		[TestCase("text/xml")]
+		[TestCase("text/json")]
+		public static void WhenAcceptHeaderIsMediaTypeThenNegotiatedMediaTypeMatches(string expected) {
+			var actual = ContentNegotiation.Negotiate(
+				new FormatterSelector(),
+				new[] { "text/xml", "text/json" },
+				new[] { expected });
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase("text/xml", 0.8, "text/json", 0.9, "text/json")]
+		[TestCase("text/xml", 0.9, "text/xml", 0.8, "text/xml")]
+		public static void WhenAcceptHeaderValuesHaveQualityThenNegotiatedMediaTypeMatchesHigherQuality(string mediaType1, double quality1, string mediaType2, double quality2, string expected) {
+			var actual = ContentNegotiation.Negotiate(
+				new FormatterSelector(),
+				new[] { "text/xml", "text/json" },
+				new[] { new MediaTypeWithQualityHeaderValue(mediaType1, quality1),
+						new MediaTypeWithQualityHeaderValue(mediaType2, quality2) });
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase("text/xml;q=0.8", "text/json;q=0.9", "text/json")]
+		[TestCase("text/xml;q=0.9", "text/xml;q=0.8", "text/xml")]
+		public static void WhenAcceptHeaderStringsHaveQualityThenNegotiatedMediaTypeMatchesHigherQuality(string mediaType1, string mediaType2, string expected) {
+			var actual = ContentNegotiation.Negotiate(
+				new FormatterSelector(),
+				new[] { "text/xml", "text/json" },
+				new[] { mediaType1, mediaType2 });
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase("text/xml;q=0.8, text/json;q=0.9", "text/json")]
+		[TestCase("text/xml;q=0.9, text/xml;q=0.8", "text/xml")]
+		public static void WhenAcceptHeaderStringHasQualityThenNegotiatedMediaTypeMatchesHigherQuality(string acceptHeader, string expected) {
+			var actual = ContentNegotiation.Negotiate(
+				new FormatterSelector(),
+				new[] { "text/xml", "text/json" },
+				acceptHeader);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ WebApi.Conneg.Web/SimpleHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.Conneg.Web {
+	public class SimpleHandler : DelegatingHandler {
+		private readonly IFormatterSelector _formatterSelector;
+		private readonly IDictionary<string, string> _formatters;
+
+		public SimpleHandler() {
+			_formatterSelector = new FormatterSelector();
+			_formatters = new Dictionary<string, string> {
+				{ "application/xml", @"<?xml version=""1.0""?>
+<root>
+	<string>{0}</string>
+</root>" },
+				{ "application/json", @"{{""result"":""{0}""}}" },
+				{ "text/html", @"<!doctype html>
+<html lang=""en"">
+	<head>
+		<meta charset=""utf-8"" />
+		<title>Simple</title>
+	</head>
+	<body>
+		<section>
+			<header>
+				<h2>Simple</h2>
+			</header>
+			{0}
+		</section>
+	</body>
+</html>" },
+			};
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+			return Task.Factory.StartNew(() => {
+				var mediaType = ContentNegotiation.Negotiate(_formatterSelector, _formatters.Keys, request.Headers.Accept);
+				var result = ApplyTemplate("value", mediaType);
+
+				var response = request.CreateResponse();
+				response.Content = new StringContent(result, Encoding.UTF8, mediaType);
+				return response;
+			}, cancellationToken);
+		}
+
+		private string ApplyTemplate(string content, string mediaType) {
+			// This could run Razor or some other template engine.
+			return string.Format(_formatters[mediaType], content);
+		}
+	}
+}
