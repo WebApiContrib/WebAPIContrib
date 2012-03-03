866ff689d1f53fc69eceae413b317edbcd0db8b5
Pedro Reys <pedroreys@gmail.com>
2012-03-03 11:14:55 -0600

    added Create Response

---
+++ src/WebApiContrib/ResponseMessages/CreateResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace WebApiContrib.ResponseMessages
+{
+    public class CreateResponse : HttpResponseMessage
+    {
+         public CreateResponse()
+         {
+             StatusCode = HttpStatusCode.Created;
+         }
+
+        public CreateResponse(IApiResource resource) :this()
+        {
+            Headers.Location = resource.Self;
+        }
+    }
+}
---
+++ src/WebApiContrib/ResponseMessages/IApiResource.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebApiContrib.ResponseMessages
+{
+    public interface IApiResource
+    {
+        Uri Self { get; }     
+    }
+}
