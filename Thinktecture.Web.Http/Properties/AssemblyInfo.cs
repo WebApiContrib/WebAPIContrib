9b86d9088024251f685121fe31528c32b26be75d
Christian Weyer <christian.weyer@thinktecture.com>
2012-03-06 11:37:15 +0100

    Updated version number to 0.1.0.0 (for NuGet)

--- Thinktecture.Web.Http/Properties/AssemblyInfo.cs
+++ Thinktecture.Web.Http/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Thinktecture.Web.Http")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Thinktecture.Web.Http library provides convenience extensions from the community for the ASP.NET Web API framework (Beta 1) - like custom media type formatters, message handlers, action filters, dependency injection helpers and more.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("thinktecture")]
 [assembly: AssemblyProduct("Thinktecture.Web.Http")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © thinktecture 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
