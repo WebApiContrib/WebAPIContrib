dd3d7e810821925e0a6df4b6f916abd2e2a9cdb6
Ali Kheyrollahi <aliostad@gmail.com>
2012-08-01 17:13:14 +0100

    adding serializer

---
+++ src/WebApiContrib/Serialization/MessageContentHttpMessageSerializer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace WebApiContrib.Serialization
+{
+	public class MessageContentHttpMessageSerializer : IHttpMessageSerializer
+	{
+		private bool _bufferContent;
+
+		public MessageContentHttpMessageSerializer()
+			: this(false)
+		{
+
+		}
+
+		public MessageContentHttpMessageSerializer(bool bufferContent)
+		{
+			_bufferContent = bufferContent;
+		}
+
+
+		public void Serialize(HttpResponseMessage response, Stream stream)
+		{
+			byte[] assuranceBuffer = null;
+			if (_bufferContent && response.Content != null)
+				assuranceBuffer = response.Content.ReadAsByteArrayAsync().Result; // make sure it is buffered
+
+			var httpMessageContent = new HttpMessageContent(response);
+			var buffer = httpMessageContent.ReadAsByteArrayAsync().Result;
+			stream.Write(buffer, 0, buffer.Length);
+		}
+
+		public void Serialize(HttpRequestMessage request, Stream stream)
+		{
+			byte[] assuranceBuffer = null;
+			if (_bufferContent && request.Content != null)
+				assuranceBuffer = request.Content.ReadAsByteArrayAsync().Result; // make sure it is buffered
+
+			var httpMessageContent = new HttpMessageContent(request);
+			var buffer = httpMessageContent.ReadAsByteArrayAsync().Result;
+			stream.Write(buffer, 0, buffer.Length);
+		}
+
+		public HttpResponseMessage DeserializeToResponse(Stream stream)
+		{
+			var response = new HttpResponseMessage();
+			var memoryStream = new MemoryStream();
+			stream.CopyTo(memoryStream);
+			response.Content = new ByteArrayContent(memoryStream.ToArray());
+			response.Content.Headers.Add("Content-Type", "application/http;msgtype=response");
+			return response.Content.ReadAsHttpResponseMessageAsync().Result;
+		}
+
+		public HttpRequestMessage DeserializeToRequest(Stream stream)
+		{
+			var request = new HttpRequestMessage();
+			var memoryStream = new MemoryStream();
+			stream.CopyTo(memoryStream);
+			request.Content = new ByteArrayContent(memoryStream.ToArray());
+			request.Content.Headers.Add("Content-Type", "application/http;msgtype=request");
+			return request.Content.ReadAsHttpRequestMessageAsync().Result;
+		}
+	}
+}
