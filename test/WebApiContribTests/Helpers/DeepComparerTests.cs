3b65db94525d37df0526628c5616888b4aa18b1d
Ali Kheyrollahi <aliostad@gmail.com>
2012-08-01 23:57:34 +0100

    DeepComparer

---
+++ test/WebApiContribTests/Helpers/DeepComparerTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using NUnit.Framework;
+
+namespace WebApiContribTests.Helpers
+{
+	[TestFixture]
+	public class DeepComparerTests
+	{
+		[Test]
+		public void TestNulls()
+		{
+			Assert.AreEqual(0, DeepComparer.Compare<string>(null, null).Count());
+		}
+
+		[Test]
+		public void TestNonNulls()
+		{
+			Assert.AreEqual(1, DeepComparer.Compare<string>(null, "").Count());
+		}
+
+		[Test]
+		public void TestPrimitive_Pass()
+		{
+			Assert.AreEqual(0, DeepComparer.Compare(6, 6).Count());
+		}
+
+		[Test]
+		public void TestPrimitive_Fail()
+		{
+			Assert.AreEqual(1, DeepComparer.Compare(7, 6).Count());
+		}
+
+		[Test]
+		public void TestNullable_Pass()
+		{
+			int? a = 6;
+			int? b = 6;
+			Assert.AreEqual(0, DeepComparer.Compare(a, b).Count());
+		}
+
+		[Test]
+		public void TestNullable_Fail()
+		{
+			int? a = 7;
+			int? b = 6;
+			Assert.AreEqual(1, DeepComparer.Compare(a, b).Count());
+		}
+
+		[Test]
+		public void TestNullable_Fail_Null()
+		{
+			int? a = null;
+			int? b = 6;
+			Assert.AreEqual(1, DeepComparer.Compare(a, b).Count());
+		}
+
+		[Test]
+		public void TestClass_Pass()
+		{
+			var a = new HttpResponseMessage();
+			var b = new HttpResponseMessage();
+			var list = DeepComparer.Compare(a, b);
+			foreach (var s in list)
+				Console.WriteLine(s);
+			
+			Assert.AreEqual(0, list.Count());
+		}
+
+		[Test]
+		public void TestClass_Fail()
+		{
+			var a = new HttpResponseMessage();
+			var b = new HttpResponseMessage( HttpStatusCode.Continue);
+			var list = DeepComparer.Compare(a, b);
+			foreach (var s in list)
+				Console.WriteLine(s);
+			Assert.AreEqual(3, list.Count());
+		}
+
+	}
+}
---
+++ test/WebApiContribTests/Serialization/SerialisationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace WebApiContribTests.Serialization
+{
+	[TestFixture]
+	public class SerialisationTests
+	{
+		[Test]
+		public void TestRequest()
+		{
+			var a = new MyStruct(){X = 5, Y = 3};
+			var b = new MyStruct(){X = 5, Y = 3};
+			Assert.AreEqual(a, b);
+			
+		}
+
+		private struct MyStruct
+		{
+			public int X;
+			public int Y;
+		}
+	}
+}
