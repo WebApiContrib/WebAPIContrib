3d6a0e8db0fcc254e9615f6c3c0728b8881295b3
Ali Kheyrollahi <aliostad@gmail.com>
2012-08-01 17:13:14 +0100

    adding serializer

---
+++ src/WebApiContrib/Serialization/IHttpMessageSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace WebApiContrib.Serialization
+{
+	public interface IHttpMessageSerializer
+	{
+		void Serialize(HttpResponseMessage response, Stream stream);
+		void Serialize(HttpRequestMessage request, Stream stream);
+		HttpResponseMessage DeserializeToResponse(Stream stream);
+		HttpRequestMessage DeserializeToRequest(Stream stream);
+	}
+
+}
