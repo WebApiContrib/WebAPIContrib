7792660bd5ea36587d5fdc0f7e0dd94c3d4d24bc
Ali Kheyrollahi <aliostad@gmail.com>
2012-08-21 00:52:01 +0100

    Changing to async. wip

---
+++ src/WebApiContrib/Serialization/IHttpMessageSerializerAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiContrib.Serialization
+{
+	public interface IHttpMessageSerializerAsync
+	{
+		Task Serialize(Task<HttpResponseMessage> response, Stream stream);
+		void Serialize(HttpRequestMessage request, Stream stream);
+		Task<HttpResponseMessage> DeserializeToResponse(Stream stream);
+		Task<HttpRequestMessage> DeserializeToRequest(Stream stream);
+
+	}
+}
