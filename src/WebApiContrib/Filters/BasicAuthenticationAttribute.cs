d9ede543f8b8832b3ac052373e2da1973cf403a8
Waldyr Felix <waldyrfelix@gmail.com>
2012-09-15 12:15:56 -0300

    ajustado atributo basic authentication

--- src/WebApiContrib/Filters/BasicAuthenticationAttribute.cs
+++ src/WebApiContrib/Filters/BasicAuthenticationAttribute.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
@@ -12,13 +13,32 @@ using WebApiContrib.MessageHandlers;
 
 namespace WebApiContrib.Filters
 {
+    public interface IBasicAuthentication
+    {
+        bool Authentication(string username, string password);
+    }
+
     public class BasicAuthenticationAttribute : ActionFilterAttribute
     {
         internal struct BasicCredentials
         {
             public string Username { get; set; }
             public string Password { get; set; }
+
+            public override string ToString()
+            {
+                return String.Format("{0}:{1}", Username, Password);
+            }
         }
+
+        public BasicAuthenticationAttribute(string realm)
+        //public BasicAuthenticationAttribute(string realm, IBasicAuthentication basicAuthentication = null)
+        {
+            //this.BasicAuthentication = basicAuthentication;
+            this.Realm = realm;
+        }
+
+        public IBasicAuthentication BasicAuthentication { get; private set; }
 
         public string Realm { get; private set; }
 
@@ -39,6 +59,8 @@ namespace WebApiContrib.Filters
             return new BasicCredentials();
         }
 
+        public MemoryCache Cache { get { return MemoryCache.Default; } }
+
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
             if (actionContext.Request.Headers.Authorization != null &&
@@ -46,9 +68,15 @@ namespace WebApiContrib.Filters
             {
                 var credentials = ParseCredentials(actionContext.Request.Headers.Authorization);
 
-                if (!Authorize(credentials.Username, credentials.Password))
+                if (!Cache.Contains(credentials.ToString()) && Authorize(credentials.Username, credentials.Password))
                 {
-
+                    if (!Cache.Contains(credentials.ToString()))
+                    {
+                        //Cache.Add(credentials.ToString(), true, new DateTimeOffset(0,0,0,0,1)));
+                    }
+                }
+                else
+                {
                     actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
                     actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
                 }
