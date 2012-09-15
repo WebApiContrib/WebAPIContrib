89c9a877cbeff9fc259505adf5ffba62406b7dc8
Waldyr Felix <waldyrfelix@gmail.com>
2012-09-15 19:45:54 -0300

    added cache in the basic authorization filter

--- src/WebApiContrib/Filters/BasicAuthenticationAttribute.cs
+++ src/WebApiContrib/Filters/BasicAuthenticationAttribute.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Caching;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
-using WebApiContrib.MessageHandlers;
 
 namespace WebApiContrib.Filters
 {
-    public interface IBasicAuthentication
-    {
-        bool Authentication(string username, string password);
-    }
-
     public class BasicAuthenticationAttribute : ActionFilterAttribute
     {
         internal struct BasicCredentials
@@ -32,58 +23,56 @@ namespace WebApiContrib.Filters
         }
 
         public BasicAuthenticationAttribute(string realm)
-        //public BasicAuthenticationAttribute(string realm, IBasicAuthentication basicAuthentication = null)
         {
-            //this.BasicAuthentication = basicAuthentication;
             this.Realm = realm;
         }
-
-        public IBasicAuthentication BasicAuthentication { get; private set; }
 
         public string Realm { get; private set; }
 
         private BasicCredentials ParseCredentials(AuthenticationHeaderValue authHeader)
         {
-            try
+            var credentials = Encoding.ASCII.GetString(Convert.FromBase64String(authHeader.Parameter)).Split(':');
+
+            return new BasicCredentials
             {
-                var credentials = Encoding.ASCII.GetString(Convert.FromBase64String(authHeader.ToString().Substring(6))).Split(':');
-
-                return new BasicCredentials
-                {
-                    Username = credentials[0],
-                    Password = credentials[1]
-                };
-            }
-            catch { }
-
-            return new BasicCredentials();
+                Username = credentials[0],
+                Password = credentials[1]
+            };
         }
 
-        public MemoryCache Cache { get { return MemoryCache.Default; } }
+        protected MemoryCache Cache { get { return MemoryCache.Default; } }
 
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            if (actionContext.Request.Headers.Authorization != null &&
-                actionContext.Request.Headers.Authorization.Scheme == "Basic")
+            if (isBasicAuthentication(actionContext))
             {
                 var credentials = ParseCredentials(actionContext.Request.Headers.Authorization);
+                if (Cache.Contains(credentials.ToString()))
+                    return;
 
-                if (!Cache.Contains(credentials.ToString()) && Authorize(credentials.Username, credentials.Password))
+                if (Authorize(credentials.Username, credentials.Password))
                 {
-                    if (!Cache.Contains(credentials.ToString()))
-                    {
-                        //Cache.Add(credentials.ToString(), true, new DateTimeOffset(0,0,0,0,1)));
-                    }
-                }
-                else
-                {
-                    actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
-                    actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
+                    Cache.Add(credentials.ToString(), true, DateTimeOffset.Now.AddMinutes(10));
+                    return;
                 }
             }
+
+            unauthorizedResponse(actionContext);
         }
 
-        private bool Authorize(string username, string password)
+        private bool isBasicAuthentication(HttpActionContext actionContext)
+        {
+            return actionContext.Request.Headers.Authorization != null &&
+                   actionContext.Request.Headers.Authorization.Scheme == "Basic";
+        }
+
+        private void unauthorizedResponse(HttpActionContext actionContext)
+        {
+            actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
+        }
+
+        public virtual bool Authorize(string username, string password)
         {
             return true;
         }
