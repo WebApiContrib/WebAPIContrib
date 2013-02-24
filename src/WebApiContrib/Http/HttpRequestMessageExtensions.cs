d7523b8953befb2b74ff325f8121f56f1b94a7de
Filip W <wojcieszyn@gmail.com>
2013-02-23 22:39:13 -0500

    added Request.IsLocal extension method

---
+++ src/WebApiContrib/Http/HttpRequestMessageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WebApiContrib.Http
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static bool IsLocal(this HttpRequestMessage request)
+        {
+            var localFlag = request.Properties["MS_IsLocal"] as Lazy<bool>;
+            return localFlag != null && localFlag.Value;
+        }
+    }
+}
