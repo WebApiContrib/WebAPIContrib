54b30f8bf61fdc5854a9b7e90815b6c61b6a7a9e
Chris Missal <chris.missal@gmail.com>
2013-02-25 23:31:21 -0600

    added nuspec files

--- src/WebApiContrib.Testing/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.Testing/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.12")]
-[assembly: AssemblyFileVersion("0.7.12")]
+[assembly: AssemblyVersion("0.7.17")]
+[assembly: AssemblyFileVersion("0.7.17")]
