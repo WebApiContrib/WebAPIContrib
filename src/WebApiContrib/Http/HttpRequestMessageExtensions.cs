f39b27413edf19f1ffb6d0e089fbd23e94e6f208
Filip W <wojcieszyn@gmail.com>
2013-05-23 12:36:26 +0300

    added GetClientIpAddress

--- src/WebApiContrib/Http/HttpRequestMessageExtensions.cs
+++ src/WebApiContrib/Http/HttpRequestMessageExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
 using System.Net.Http;
 
 namespace WebApiContrib.Http
 {
+    private const string HttpContext = "MS_HttpContext";
+    private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+    
     public static class HttpRequestMessageExtensions
     {
         public static bool IsLocal(this HttpRequestMessage request)
@@ -11,4 +14,27 @@ namespace WebApiContrib.Http
             return localFlag != null && localFlag.Value;
         }
     }
+    
+    public static string GetClientIpAddress(this HttpRequestMessage request)
+    {
+        if (request.Properties.ContainsKey(HttpContext))
+        {
+            dynamic ctx = request.Properties[HttpContext];
+            if (ctx != null)
+            {
+                return ctx.Request.UserHostAddress;
+            }
+        }
+
+        if (request.Properties.ContainsKey(RemoteEndpointMessage))
+        {
+            dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+            if (remoteEndpoint != null)
+            {
+                return remoteEndpoint.Address;
+            }
+        }
+
+        return null;
+    }    
 }
