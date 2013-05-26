1891b16cee1cb7ec25fde9bebfd3c2419c20a30d
Chris Missal <chris.missal@gmail.com>
2013-05-25 20:52:13 -0500

    fixed compilation error

--- src/WebApiContrib/Http/HttpRequestMessageExtensions.cs
+++ src/WebApiContrib/Http/HttpRequestMessageExtensions.cs
@@ -3,38 +3,38 @@ using System.Net.Http;
 
 namespace WebApiContrib.Http
 {
-    private const string HttpContext = "MS_HttpContext";
-    private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
-    
     public static class HttpRequestMessageExtensions
     {
+        private const string HttpContext = "MS_HttpContext";
+        private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+
         public static bool IsLocal(this HttpRequestMessage request)
         {
             var localFlag = request.Properties["MS_IsLocal"] as Lazy<bool>;
             return localFlag != null && localFlag.Value;
         }
+
+        public static string GetClientIpAddress(this HttpRequestMessage request)
+        {
+            if (request.Properties.ContainsKey(HttpContext))
+            {
+                dynamic ctx = request.Properties[HttpContext];
+                if (ctx != null)
+                {
+                    return ctx.Request.UserHostAddress;
+                }
+            }
+
+            if (request.Properties.ContainsKey(RemoteEndpointMessage))
+            {
+                dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+                if (remoteEndpoint != null)
+                {
+                    return remoteEndpoint.Address;
+                }
+            }
+
+            return null;
+        }
     }
-    
-    public static string GetClientIpAddress(this HttpRequestMessage request)
-    {
-        if (request.Properties.ContainsKey(HttpContext))
-        {
-            dynamic ctx = request.Properties[HttpContext];
-            if (ctx != null)
-            {
-                return ctx.Request.UserHostAddress;
-            }
-        }
-
-        if (request.Properties.ContainsKey(RemoteEndpointMessage))
-        {
-            dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
-            if (remoteEndpoint != null)
-            {
-                return remoteEndpoint.Address;
-            }
-        }
-
-        return null;
-    }    
 }
