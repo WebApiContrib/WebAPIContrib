f59611b61f7040fae7b24382142c69e21febb60a
Maarten Balliauw <maarten@balliauw.be>
2013-05-28 16:33:26 +0200

    Removed GetClientIpAddress extension method as it already exists in WebApiContrib

--- src/WebApiContrib/MessageHandlers/ThrottlingHandler.cs
+++ src/WebApiContrib/MessageHandlers/ThrottlingHandler.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.ServiceModel.Channels;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WebApiContrib.Caching;
+using WebApiContrib.Http;
 
 namespace WebApiContrib.MessageHandlers
 {
-
     public class ThrottlingHandler
         : DelegatingHandler
     {
