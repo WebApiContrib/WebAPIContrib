36f65ba63f74bbfcc4e640d741bd7f238388780d
Chris Missal <chris.missal@gmail.com>
2013-05-28 10:01:05 -0500

    version rev

--- src/WebApiContrib/Properties/AssemblyInfo.cs
+++ src/WebApiContrib/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.11")]
-[assembly: AssemblyFileVersion("0.9.11")]
+[assembly: AssemblyVersion("0.9.12")]
+[assembly: AssemblyFileVersion("0.9.12")]
 
 [assembly: InternalsVisibleTo("WebApiContribTests")]
 [assembly: InternalsVisibleTo("Rhino.Mocks")]
