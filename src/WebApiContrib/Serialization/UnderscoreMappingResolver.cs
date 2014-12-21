be8fbb3e555b1062774360a9e2ddd0a114db7657
Alexander Batishchev <abatishchev@gmail.com>
2014-12-20 23:24:16 -0800

    Adding UnderscoreMappingResolver with test

---
+++ src/WebApiContrib/Serialization/UnderscoreMappingResolver.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebApiContrib.Serialization
+{
+    public class UnderscoreMappingResolver : Newtonsoft.Json.Serialization.DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return Regex.Replace(propertyName, "([A-Z])([A-Z][a-z])|([a-z0-9])([A-Z])", "$1$3_$2$4", RegexOptions.Compiled).ToLower();
+        }
+    }
+}
