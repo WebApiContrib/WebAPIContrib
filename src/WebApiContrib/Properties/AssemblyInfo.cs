b8944841605ca37c4d30dab507451c7eac609bd9
Chris Missal <chris.missal@gmail.com>
2015-01-02 17:01:28 -0600

    bumps version to 2.0

--- src/WebApiContrib/Properties/AssemblyInfo.cs
+++ src/WebApiContrib/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.14")]
-[assembly: AssemblyFileVersion("0.9.14")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
 [assembly: InternalsVisibleTo("WebApiContribTests")]
 [assembly: InternalsVisibleTo("Rhino.Mocks")]
