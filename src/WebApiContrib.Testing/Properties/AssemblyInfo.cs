da3d2b7457aa6f1f7c1d58626319e54e8ec1a559
Alex Batishchev <alexbat@microsoft.com>
2016-02-17 14:37:22 -0800

    Bumping version to 2.0.0, updating build scripts, nuspecs

--- src/WebApiContrib.Testing/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.Testing/Properties/AssemblyInfo.cs
@@ -4,10 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("WebApiContrib.Testing")]
 [assembly: AssemblyProductAttribute("WebApiContrib")]
 [assembly: AssemblyDescriptionAttribute("Community Contributions for ASP.NET Web API")]
-[assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0")]
+[assembly: AssemblyCopyrightAttribute("WebApiContrib Contributors")]
+[assembly: AssemblyVersionAttribute("2.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.0.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.0";
+        internal const string Version = "2.0.0";
     }
 }
